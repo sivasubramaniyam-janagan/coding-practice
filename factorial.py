fda4751bfed4fbef32343c4ad8f2a90a68418eba
sivasubramaniyam-janagan <sivasubramaniyamjanagan@gmail.com>
2026-05-13 04:58:30 +0530

    factorial

---
+++ factorial.py
@@ -0,0 +1,24 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def factorial(n):
+    if(n==1 or n==0):
+        return 1;
+    else :
+        return n*factorial(n-1)
+
+
+if __name__ == '__main__':
+   
+
+    n = int(input().strip())
+
+    result = factorial(n)
+    
+
+
